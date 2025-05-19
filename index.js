11bdaec602f691de72a4a889b3df1b407439607b
andrjre <andrjre@gmail.com>
2025-05-19 19:13:19 +0100

    Can cooperate and defect.

--- index.js
+++ index.js
@@ -1,23 +1,36 @@
 let player = document.querySelector("#player");
 let computer = document.querySelector("#computer");
 let scoreboard = document.querySelectorAll(".scoreboard")
+let cooperate = document.querySelector("#cooperate")
+let defect = document.querySelector("#defect")
 
 for(let i = 0;i < 10; i ++){
     let point = document.createElement("div")
     point.className = "point"
     player.appendChild(point)
-    point.id = `playerPoint ${i}`
+    point.id = `playerPoint${i}`
 }
 
 for(let i = 0;i < 10; i ++){
     let point = document.createElement("div")
     point.className = "point"
     computer.appendChild(point)
-    point.id = `computerPoint ${i}`
+    point.id = `computerPoint${i}`
 }
 
+let num = 0
 
-let cooperate
-let defect
-let points 
+cooperate.addEventListener("click", function(){
+    console.log("cooperate")
+    let current = document.querySelector(`#playerPoint${num}`)
+    current.style.backgroundColor = "green"
+    num++
+})
+
+defect.addEventListener("click", function(){
+    console.log("defect")
+    let current = document.querySelector(`#playerPoint${num}`)
+    current.style.backgroundColor = "red"
+    num++
+})
 
